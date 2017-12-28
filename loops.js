a32ef8d3d5fa98a9dc3525aeda6c2ca254bc7bf7
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-28 11:35:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function  forLoop(array) {
   for (var i = 0; i < array.length; i++) {
-    if ([i] == 1) {
+    if ([i] = 1) {
       return "I am 1 strange loop."
     } else {
       return ("I am ${i} strange loops")
