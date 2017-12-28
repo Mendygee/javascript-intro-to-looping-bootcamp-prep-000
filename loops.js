3c3bbcb55a43cc52effd37360eb5e3b3e5cfe026
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-28 11:35:14 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,6 +3,6 @@ function  forLoop(array) {
     if ([i] == 1) {
       return "I am 1 strange loop."
     } else {
-      return ("I am ${i} strange loop")
+      return ("I am ${i} strange loops")
     }
 }
