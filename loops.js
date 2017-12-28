a396675301b6427ecd682f7cff3fb1be66bcdf80
Mendy Goorevitch <menachemgoorevitch@gmail.com>
2017-12-28 12:03:21 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,44 +1,28 @@
-/*
 function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.")
+  for (var i = 0; i < 25; i++) {
+    if (i == 1) {
+      array.push ("I am 1 strange loop.")
     } else {
-      array.push(`I am ${i} strange loops.`)
+      array.push  ("I am ${i} strange loops")
     }
   }
-
-  return array
-}
-*/
-
-function forLoop(array) {
-  for (let i = 0; i < 25; i++) {
-    if (i === 1) {
-      array.push("I am 1 strange loop.")
-    } else {
-      array.push(`I am ${i} strange loop${i === 0 ? '' : 's'}.`)
-    }
-  }
-
-  return array
+return array;
 }
 
 function whileLoop(n) {
   while (n > 0) {
-    console.log(--n);
+    console.log (--n)
   }
-  return 'done'
+  return "done";
 }
 
 function doWhileLoop(array) {
   function maybeTrue() {
-    return Math.random() >= 0.5
-  }
+  return Math.random() >= 0.5;
+}
 
-  do {
-    array = array.slice(1)
-  } while (array.length > 0 && maybeTrue())
-
-  return array
+do {
+  array = array.slice(1);
+} while (array.length > 0 && maybeTrue());
+return array;
 }
